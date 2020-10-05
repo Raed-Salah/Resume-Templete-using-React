0c8cfe312ab3603c36c260a67120fe3f2993c81b
Raed Salah <Raed-Salah@github.com>
2020-10-05 07:15:23 +0200

    Updates

---
+++ precache-manifest.f533594d67e72c1a3c845616e1e4650e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b719cab94513839b1aabdb2dad636d",
+    "url": "/Resume-Templete-using-React/index.html"
+  },
+  {
+    "revision": "3a911dfcb54c564c9671",
+    "url": "/Resume-Templete-using-React/static/css/main.6f1c3a9a.chunk.css"
+  },
+  {
+    "revision": "232af952b072402f7c73",
+    "url": "/Resume-Templete-using-React/static/js/2.ce4ad80e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Resume-Templete-using-React/static/js/2.ce4ad80e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a911dfcb54c564c9671",
+    "url": "/Resume-Templete-using-React/static/js/main.c99a815d.chunk.js"
+  },
+  {
+    "revision": "32e249b49c49284ac0e4",
+    "url": "/Resume-Templete-using-React/static/js/runtime-main.8d0e5242.js"
+  }
+]);
